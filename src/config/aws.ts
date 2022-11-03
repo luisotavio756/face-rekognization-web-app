7b86e234b87f30a384a16ba6743576b730dd585e
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-11-03 17:30:40 -0300

    Fixing env var

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -2,7 +2,7 @@ import { ConfigurationOptions } from 'aws-sdk';
 
 const configOptions: ConfigurationOptions = {
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
   region: process.env.AWS_REGION_NAME,
 };
 
